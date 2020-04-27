9f85b5e5f28b368b36a3c04243b8c874fe1f5cfc
Raul Garcia <punkvomit21@gmail.com>
2020-04-26 22:22:57 -0400

    added render and render error functions to bookmarks js

--- src/bookmarks.js
+++ src/bookmarks.js
@@ -1,1 +1,54 @@
+/* global $ */
 import store from './store';
+import api from './api';
+
+function render(bookId, expandedView) {
+  renderError();
+
+  if (store.addingBook) {
+    $('main').html(generateAddBookmarkView());
+    let view = 'addingBook';
+    generateCodeBlock(view);
+  }
+
+  else if (store.filterBook !== 0 && !bookId) {
+    let html = [generateInitialView(), generateFilteredResults(store.filterBook)].join('');
+    $('main').html(html);
+    let view = 'filterBook';
+    generateCodeBlock(view);
+  }
+
+  else if (store.editBook) {
+    let html = generateEditView(bookId);
+    $('main').html(html);
+    let view = 'editingBook';
+    generateCodeBlock(view);
+  }
+
+  else if (expandedView !== undefined) {
+    let html = generateExpandedView(bookId, expandedView);
+    $(expandedView).html(html);
+    let view = 'expandedBookView';
+    generateCodeBlock(view);
+  }
+
+  else {
+    let html = [generateInitialView(), generateBook()].join('');
+    $('main').html(html);
+    let view = 'initialView';
+    generateCodeBlock(view);
+  }
+}
+
+function renderError() {
+  if (store.error.code) {
+    $('div.error-container').html(`${store.error.message}`);
+    let view = 'error';
+    generateCodeBlock(view);
+  }
+  else {
+    $('div.error-container').empty();
+  }
+}
+
+
